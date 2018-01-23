4650b98750d2ae5630152099e9ee4fc02afb5ec9
klinson <klinson@163.com>
2018-01-24 00:40:54 +0800

    附件上传

--- pages/create/create.js
+++ pages/create/create.js
@@ -72,6 +72,33 @@ Page({
       type: e.detail.value
     });
   },
+  selectCover: function (e) {
+    var _this = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths);
+        wx.uploadFile({
+          url: app.globalData.domain + 'upload', //仅为示例，非真实的接口地址
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {},
+          success: function (res) {
+            console.log(res);
+            let data = JSON.parse(res.data);
+            _this.setData({
+              'cover': data.data.path
+            })
+            //do something
+          }
+        })
+      }
+    })
+  },
   selectMap: function (e) {
     var _this = this;
     var latitude = e.target.dataset.latitude || '';
