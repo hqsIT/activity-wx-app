7bf1dd6f2d229daf8e0c7415c520d94ec5e9ce0d
klinson <klinson@163.com>
2018-01-28 02:05:13 +0800

    报名状态

--- pages/activity/detail/detail.js
+++ pages/activity/detail/detail.js
@@ -1,126 +1,140 @@
 var app = getApp();
-var setTitle = function() {
-	wx.setNavigationBarTitle({
-		title: '查看详情'
-	});
+var setTitle = function () {
+  wx.setNavigationBarTitle({
+    title: '查看详情'
+  });
 };
-var setStatus = function(status) {
+var setStatus = function (status, tip) {
 
-	if (status) {
-		this.setData({
-			baoMin: 'hide',
-			baoMinChengGong: ''
-		});
-	} else {
-		this.setData({
-			baoMin: '',
-			baoMinChengGong: 'hide'
-		});
-	}
+  if (status) {
+    this.setData({
+      baoMin: 'hide',
+      baoMinChengGong: ''
+    });
+  } else {
+    this.setData({
+      baoMin: '',
+      baoMinChengGong: 'hide',
+      tip: tip
+    });
+  }
 };
 Page({
-	data: {
-		disabled: true
-	},
-	onLaunch:function(){
-		wx.showNavigationBarLoading();
-	},
-	onShow: function() {
-		setTitle();
-		
-	},
-	tel: function(e) {
-		var phoneNumber = e.target.dataset.tel;
-		
-		if (!phoneNumber) {
-			return;
-		}
-		wx.makePhoneCall({
-			phoneNumber: phoneNumber
-		});
-	},
-	join: function(e) {
-		var id = e.target.dataset.id;
+  data: {
+    disabled: true,
+    tip: '已经报名了'
+  },
+  onLaunch: function () {
+    wx.showNavigationBarLoading();
+  },
+  onShow: function () {
+    setTitle();
 
-		
-		var _this = this;
-		//调用应用实例的方法获取全局数据
+  },
+  tel: function (e) {
+    var phoneNumber = e.target.dataset.tel;
+
+    if (!phoneNumber) {
+      return;
+    }
+    wx.makePhoneCall({
+      phoneNumber: phoneNumber
+    });
+  },
+  join: function (e) {
+    var id = e.target.dataset.id;
+
+
+    var _this = this;
+    //调用应用实例的方法获取全局数据
     wx.request({
       url: app.globalData.domain + 'activity/enroll/' + id,
+      header: {
+        'cookie': 'laravel_session=' + wx.getStorageSync('authorization')
+      },
       data: {
-        
+
       },
       method: 'GET',
       success: function (res) {
-        // setStatus.call(_this, true);
+        console.log(res)
         if (res.data.code) {
           wx.showToast({
             title: res.data.message,
-            icon: 'error',
+            image: '/image/about.png',
             duration: 2000
           });
+          setStatus.call(_this, true, res.data.message);
+
         } else {
           wx.showToast({
             title: res.data.message,
             icon: 'success',
             duration: 2000
           });
+          setStatus.call(_this, true, '报名成功');
         }
       },
       fail: function () { },
       complete: function () { }
     });
-	},
-	onLoad: function(options) {
+  },
+  onLoad: function (options) {
 
-		var _this = this;
+    var _this = this;
 
-		wx.request({
-			url: app.globalData.domain + 'activity/detail/' + options.id,
-			data: {
-				
-			},
-			method: 'GET',
-			success: function(res) {
+    wx.request({
+      url: app.globalData.domain + 'activity/detail/' + options.id,
+      header: {
+        'cookie': 'laravel_session=' + wx.getStorageSync('authorization')
+      },
+      data: {
+
+      },
+      method: 'GET',
+      success: function (res) {
         console.log(res);
         _this.setData(res.data.data);
-			}
-		});
-		// //avatarUrl
-		// wx.request({
-		// 	url: app.globalData.domain + 'api/wxapp/activity-user?id=' + options.id,
-		// 	data: {
-		// 		time: app.globalData.getTime()
-		// 	},
-		// 	method: 'GET',
-		// 	success: function(res) {
+        if (res.data.data.activity_status == 0) {
+          setStatus.call(_this, true, res.data.data.tip);
+        }
+      }
+    });
+    // //avatarUrl
+    // wx.request({
+    // 	url: app.globalData.domain + 'api/wxapp/activity-user?id=' + options.id,
+    // 	data: {
+    // 		time: app.globalData.getTime()
+    // 	},
+    // 	method: 'GET',
+    // 	success: function(res) {
 
-		// 		_this.setData({
-		// 			users: res.data
-		// 		});
-				
-		// 	}
-		// });
+    // 		_this.setData({
+    // 			users: res.data
+    // 		});
 
-		// app.getUserInfo(function(userInfo) {
-		// 	wx.request({
-		// 		url: app.globalData.domain + 'api/wxapp/activity-user?id=' + options.id,
-		// 		data: {
-		// 			time: app.globalData.getTime()
-		// 		},
-		// 		success: function(res) {
-		// 			if (res && res.data && res.data.length) {
-		// 				res.data.forEach(function(item) {
-		// 					if (item.name === userInfo.nickName) {
-		// 						setStatus.call(_this, true);
-		// 					}
-		// 				});
-		// 			}
-		// 		}
-		// 	});
-		// });
+    // 	}
+    // });
 
-		setStatus.call(_this, false);
-		setTitle();
-	}
+    // app.getUserInfo(function(userInfo) {
+    // 	wx.request({
+    // 		url: app.globalData.domain + 'api/wxapp/activity-user?id=' + options.id,
+    // 		data: {
+    // 			time: app.globalData.getTime()
+    // 		},
+    // 		success: function(res) {
+    // 			if (res && res.data && res.data.length) {
+    // 				res.data.forEach(function(item) {
+    // 					if (item.name === userInfo.nickName) {
+    // 						setStatus.call(_this, true);
+    // 					}
+    // 				});
+    // 			}
+    // 		}
+    // 	});
+    // });
+
+    setStatus.call(_this, false);
+    setTitle();
+  }
 });
