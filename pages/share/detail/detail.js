e23963de8d8855812cdaf06036f4e4b0c784ca9b
klinson <klinson@163.com>
2018-02-06 00:19:48 +0800

    点赞取消赞实现

--- pages/share/detail/detail.js
+++ pages/share/detail/detail.js
@@ -1,66 +1,122 @@
-// pages/share/detail/detail.js
+const app = getApp();
+const https = app.https;
+const util = app.util;
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    info: [],
+    isFavoured: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+    const _this = this;
+    https.GET({
+      url: 'share/detail/' + options.id,
+      success: function (res) {
+        console.log(res);
+        _this.setData({
+          info: res.data.data.info,
+          isFavoured: res.data.data.isFavoured
+        });
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  favour: function (e) {
+    var id = e.currentTarget.dataset.id;
+    const _this = this;
+    https.GET({
+      url: 'share/favour/' + id,
+      success: function (res) {
+        console.log(res)
+        if (res.data.code) {
+          wx.showToast({
+            title: res.data.message,
+            image: '/image/about.png',
+            duration: 2000
+          });
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'success',
+            duration: 2000
+          });
+          _this.setData({
+            isFavoured: 1
+          });
+        }
+      },
+      fail: function () { },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  unFavour: function (e) {
+    var id = e.currentTarget.dataset.id;
+    const _this = this;
+    https.GET({
+      url: 'share/unFavour/' + id,
+      success: function (res) {
+        console.log(res)
+        if (res.data.code) {
+          wx.showToast({
+            title: res.data.message,
+            image: '/image/about.png',
+            duration: 2000
+          });
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'success',
+            duration: 2000
+          });
+          _this.setData({
+            isFavoured: 0,
+          });
+        }
+      },
+      fail: function () { },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+  join: function (e) {
+    var id = e.currentTarget.dataset.id;
+    const _this = this;
+    //调用应用实例的方法获取全局数据
+    https.GET({
+      url: 'activity/enroll/' + id,
+      success: function (res) {
+        console.log(res)
+        if (res.data.code) {
+          wx.showToast({
+            title: res.data.message,
+            image: '/image/about.png',
+            duration: 2000
+          });
+          _this.setStatus(true, res.data.message);
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+        } else {
+          wx.showToast({
+            title: res.data.message,
+            icon: 'success',
+            duration: 2000
+          });
+          _this.setStatus(true, '报名成功');
+        }
+      },
+      fail: function () { },
+    });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  setStatus: function (status, tip) {
+    if (status) {
+      this.setData({
+        baoMin: 'hide',
+        baoMinChengGong: ''
+      });
+    } else {
+      this.setData({
+        baoMin: '',
+        baoMinChengGong: 'hide',
+        tip: tip
+      });
+    }
   }
-})
+});
